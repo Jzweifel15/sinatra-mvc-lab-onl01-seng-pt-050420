af9e2a82c28a2453cede47a1bd18a7f18c30fa86
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-24 05:17:52 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,34 +1,29 @@
 class PigLatinizer 
 
-  def split(word)
-    alphabet = ("A".."z").to_a
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    consonants = alphabet - vowels
-    unusable = ["i", "a", "an", "in", "on", "and"]
-    word.gsub!(/[^a-z0-9\s]/i, '')
-    word = word.split("")
-    # ["k", "i", "t", "t", "e", "n"]
+  def piglatinize(str)
+    a = str.split(" ")
+    b = a.map do |word|
+      piglatinize_word(word)
+    end
+    b.join(" ")
+  end
 
-    if unusable.include?(word.join) || word.size <= 1
-      word.join
-    elsif vowels.include?(word[0]) && word.size > 1
-      word.join + "ay"
-    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
-      word = word.rotate.rotate.rotate << "ay"
-      word.join
-    elsif consonants.include?(word[0] && word[1])
-      word.rotate.rotate.join + "ay"
+  def piglatinize_word(word)
+    first_letter = word[0].downcase
+
+    if ["a", "e", "i", "o", "u"].include?(first_letter)
+      "#{word}way"
     else
-      word.rotate.join + "ay"
+      consonants = []
+      consonants.push(word[0])
+      if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+        consonants.push(word[1])
+        if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+          consonants.push(word[2])
+        end
+      end
+      "#{word[consonants.length..-1]}" + "#{consonants.join}" + "ay"
     end
-
-end
-
-  def piglatinize(words)
-    result = words.split(" ").map do |word|
-      split(word)
-    end
-    result.join(" ")
   end
 
 end
