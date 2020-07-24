ea446201d3d9234c27175e7af325631a792d3715
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-24 04:31:20 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,34 @@
 class PigLatinizer 
 
-  attr_reader :word 
-  
   def piglatinize(word)
-    @word = word.split("")
-    vowels = ["a", "e", "i", "o", "u"]
-    
-    if vowels.include?(@word[0])
-      piglatinized = vowel_beginning.join("")
+    alphabet = ("A".."z").to_a
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    consonants = alphabet - vowels
+    unusable = ["i", "a", "an", "in", "on", "and"]
+    word.gsub!(/[^a-z0-9\s]/i, '')
+    word = word.split("")
+    # ["k", "i", "t", "t", "e", "n"]
+
+    if unusable.include?(word.join) || word.size <= 1
+      word.join
+    elsif vowels.include?(word[0]) && word.size > 1
+      word.join + "ay"
+    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+      word = word.rotate.rotate.rotate << "ay"
+      word.join
+    elsif consonants.include?(word[0] && word[1])
+      word.rotate.rotate.join + "ay"
     else
-      piglatinized = consonant_beginning.join("")
+      word.rotate.join + "ay"
     end
-    
-    piglatinized
-    
-  end
-  
-  def vowel_beginning
-    word = @word.push("way")
-  end
-  
-  def consonant_beginning
-    
+
+end
+
+  def to_pig_latin(words)
+    result = words.split(" ").map do |word|
+      piglatinize(word)
+    end
+    result.join(" ")
   end
 
 end
